8b98e966ac6264f3e580bfaf44cda625f1e59495
Daniel <50190195+DanielBullimore@users.noreply.github.com>
2026-08-01 00:31:00 +0000

    UDP & TCP ouboud ports
    I have added separate ports lists for the two common packet types.
    you can disable a ports list by setting is value false in conf file.

--- mvColab.sh
+++ mvColab.sh
@@ -2,7 +2,7 @@
 ##################################################################################
 # MIT License
 #
-# Copyright 26/07/2026 Daniel Bullimore
+# Copyright 01/08/2026 Daniel Bullimore
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -37,45 +37,65 @@ mvCheck10() {
 		nft add table inet mvcolab
 		nft delete table inet mvcolab;
 	elif [[ "Connected" = $(mullvad status -v | head -1) ]]; then
-	  #get table list and check if mvcolab table exists, only act if it does not.
+		#get table list and check if mvcolab table exists, only act if it does not.
 		if ! [[ "table inet mvcolab" = $(nft list tables | grep "mvcolab") ]] ; then
 			#load config file
 			SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null && pwd)"
-			 #is configured for gid based permissions
-			ALLOW_GID=  `head -1 $SCRIPT_DIR/mvColab.conf | cut -d'=' -f2`
-			 #is configured for allowed hosts only
+			ALLOW_GID=`head -1  $SCRIPT_DIR/mvColab.conf | cut -d'=' -f2`
+
 			ALLOW_HOSTS=`head -2 $SCRIPT_DIR/mvColab.conf | tail -n 1 | cut -d'=' -f2`
 			 #list of allowed outbound ports
-			OUT_PORTS=  `head -3 $SCRIPT_DIR/mvColab.conf | tail -n 1 | cut -d'=' -f2`
-			gid=`echo $(getent group mvpn) | cut -d':' -f3`
+			TCP_OUT="`head -3 $SCRIPT_DIR/mvColab.conf | tail -n 1  | cut -d'=' -f2`"
+			UDP_OUT="`head -4 $SCRIPT_DIR/mvColab.conf | tail -n 1 | cut -d'=' -f2`"
+
+			#is configured for gid based permissions
 			if [ "true" = $ALLOW_GID ];  then
+				gid=`echo $(getent group mvpn) | cut -d':' -f3`
 				echo 'add rule inet mvcolab output oif "wg0-mullvad" meta skgid != $allowGid reject comment "           (GID: allow only mvpn user group)"' | cat > $SCRIPT_DIR/mvColabGID
 				else
 				`echo "#" | cat > $SCRIPT_DIR/mvColabGID`
+				gid=0
 			fi
-			#is configured for allowed hosts only
-			if [ "true" = $ALLOW_HOSTS ]; then
-				echo 'add rule inet mvcolab output oif "wg0-mullvad" ip daddr != $allowedHosts reject comment "           (Allow Hosts: allow only mvpn user group)"' | cat > $SCRIPT_DIR/mvColabURL
+			 #is configured for allowed hosts only
+			 if [ "true" = $ALLOW_HOSTS ]; then
+				echo 'add rule inet mvcolab output oif "wg0-mullvad" ip daddr != $allowedHosts reject comment "           (Allow Hosts: allow only mvpn user group)"' | cat > $SCRIPT_DIR/mvColabDOM
 				#generate ip list of allowed hosts
 				ALLOWED_HOSTS="{"
 				while FILE= read -r line; do
 					ip=`dig +short $line | tail -n 1`
 					ALLOWED_HOSTS+="$ip,"
-				done < $SCRIPT_DIR/mvAllowUrl
+				done < $SCRIPT_DIR/mvAllow
 				ALLOWED_HOSTS+="}"
 			else
+				ALLOWED_HOSTS="{}"
 				echo '#' | cat > $SCRIPT_DIR/mvColabURL
 			fi
-			
-		  #grab the dns server ip
-		  resolver=`echo $(cat /etc/resolv.conf | head -1) | cut -d' ' -f2`
-		  #split the wireguard tunnel details up
+			#is configured for outbound tcp ports
+			if ! [ "false" = "$TCP_OUT" ]; then
+				echo 'add rule inet mvcolab output oif "wg0-mullvad" tcp dport { $tcpOutPorts } accept comment "               (TCP: Allow outbound ports )"' | cat > $SCRIPT_DIR/mvColabTCP
+			else
+				#is not configured
+				TCP_OUT="{}"
+				echo "#" | cat > $SCRIPT_DIR/mvColabTCP
+			fi
+			#is configured for outbound udp
+			if ! [ "false" = $UDP_OUT ]; then
+				echo 'add rule inet mvcolab output oif "wg0-mullvad" udp dport { $udpOutPorts } accept comment "               (UDP: Allow outbound ports )"' | cat > $SCRIPT_DIR/mvColabUDP
+			else
+				#is not configed
+				UDP_OUT="{}"
+				echo "#" | cat > $SCRIPT_DIR/mvColabUDP		
+			fi
+			#grab the dns server ip
+			resolver=`echo $(cat /etc/resolv.conf | head -1) | cut -d' ' -f2`
+			#split the wireguard tunnel details up
 			status=`echo $(mullvad status -v | head -2) | cut -d'(' -f2 | cut -d'/' -f1`
 			ip=`echo $status | cut -d':' -f1`
 			port=`echo $status | cut -d':' -f2 | cut -d'/' -f1`
 			protocol=`echo $status | cut -d':' -f2 | cut -d'/' -f2`
-		  #add security layer firewall with tunnel details
-			nft -I $SCRIPT_DIR -f $SCRIPT_DIR/mvcolabNFT -D wireguard=$ip -D wgPort=$port -D wgDNS=$resolver -D allowGid=$gid -D outPorts="$OUT_PORTS" -D allowedHosts=$ALLOWED_HOSTS;
+
+			#add security layer firewall with tunnel details
+			nft -I $SCRIPT_DIR -f $SCRIPT_DIR/mvColabNFT -D wireguard=$ip -D wgPort=$port -D wgDNS=$resolver -D allowGid=$gid -D tcpOutPorts="$TCP_OUT" -D udpOutPorts="$UDP_OUT" -D allowedHosts=$ALLOWED_HOSTS;
 		fi
 	fi
 }
